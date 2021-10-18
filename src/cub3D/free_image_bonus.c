68b6cf2a33af1f683ab54656694b5a25d9d4aca9
Bryan Ledda <louchebem06@gmail.com>
2021-10-18 06:02:12 +0200

    Update free image

--- src/cub3D/free_image_bonus.c
+++ src/cub3D/free_image_bonus.c
@@ -6,11 +6,12 @@
 /*   By: bledda <bledda@student.42nice.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/17 17:35:58 by bledda            #+#    #+#             */
-/*   Updated: 2021/10/17 23:52:55 by bledda           ###   ########.fr       */
+/*   Updated: 2021/10/18 06:01:23 by bledda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../header/cub3d.h"
+#include "../../header/cub_bonus.h"
 
 static void	free_img_sprite(t_cub *cub)
 {
@@ -21,16 +22,18 @@ static void	free_img_sprite(t_cub *cub)
 	mlx_destroy_image(cub->win.mlx, cub->sprite.circle.img);
 	mlx_destroy_image(cub->win.mlx, cub->sprite.lit.img);
 	mlx_destroy_image(cub->win.mlx, cub->sprite.tree.img);
-	
 }
 
-void	free_image(t_cub *cub)
+static void	free_img_classic(t_cub *cub)
 {
-	free_img_sprite(cub);
 	mlx_destroy_image(cub->win.mlx, cub->texture.no.img);
 	mlx_destroy_image(cub->win.mlx, cub->texture.so.img);
 	mlx_destroy_image(cub->win.mlx, cub->texture.we.img);
 	mlx_destroy_image(cub->win.mlx, cub->texture.ea.img);
+}
+
+static void	free_img_wall(t_cub *cub)
+{
 	mlx_destroy_image(cub->win.mlx, cub->texture.two.img);
 	mlx_destroy_image(cub->win.mlx, cub->texture.three.img);
 	mlx_destroy_image(cub->win.mlx, cub->texture.four.img);
@@ -45,6 +48,10 @@ void	free_image(t_cub *cub)
 	mlx_destroy_image(cub->win.mlx, cub->texture.redline.img);
 	mlx_destroy_image(cub->win.mlx, cub->texture.stairs.img);
 	mlx_destroy_image(cub->win.mlx, cub->texture.bet.img);
+}
+
+static void	free_img_shooter(t_cub *cub)
+{
 	mlx_destroy_image(cub->win.mlx, cub->shooter.first.img);
 	mlx_destroy_image(cub->win.mlx, cub->shooter.recharge3.img);
 	mlx_destroy_image(cub->win.mlx, cub->shooter.recharge1.img);
@@ -52,4 +59,12 @@ void	free_image(t_cub *cub)
 	mlx_destroy_image(cub->win.mlx, cub->shooter.viser.img);
 	mlx_destroy_image(cub->win.mlx, cub->shooter.viser_tirer.img);
 	mlx_destroy_image(cub->win.mlx, cub->shooter.tirer.img);
+}
+
+void	free_image(t_cub *cub)
+{
+	free_img_sprite(cub);
+	free_img_classic(cub);
+	free_img_wall(cub);
+	free_img_shooter(cub);
 }
