c815570525b3cf000b96e08ebb3632195ea3c16e
Ledda Bryan <louchebem06@gmail.com>
2021-10-25 01:25:43 +0200

    a

--- src/cub3D/sprite_bonus.c
+++ src/cub3D/sprite_bonus.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   sprite_bonus.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mmehran <mmehran@student.42nice.fr>        +#+  +:+       +#+        */
+/*   By: bledda <bledda@student.42nice.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/17 23:59:13 by bledda            #+#    #+#             */
-/*   Updated: 2021/10/24 22:54:50 by mmehran          ###   ########.fr       */
+/*   Updated: 2021/10/25 01:15:27 by bledda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,6 +37,11 @@ static void	put_img(t_img *dest, t_img *src, t_position center, float scale)
 	}
 }
 
+static float ft_dot(t_position a, t_position b)
+{
+	return (a.x * b.x + a.y * b.y);
+}
+
 void	sprite(t_cub *cub)
 {
 	t_player *p = &cub->player;
@@ -46,8 +51,40 @@ void	sprite(t_cub *cub)
 	t_position dir =  {cosf(cub->player.angle), sinf(cub->player.angle)};
 	float s_angle = atan2f(dsprite.y, dsprite.x);
 	float p_angle = p->angle;
-	float diff_angle = s_angle - p_angle;
+	float diff_angle = fmaxf(s_angle, p_angle) - fminf(s_angle, p_angle);
+	float dot = (dir.x * dsprite.x + dir.y * dsprite.y) / dist;
+	float cam_dist = dist * cosf(s_angle + M_PI / 2);
+	t_position udsprite =  {dsprite.x / dist, dsprite.y / dist};
+	t_position plane = {dir.x * cosf(M_PI / 2) - dir.y * sinf(M_PI / 2),
+						dir.x * sinf(M_PI / 2) + dir.y * cosf(M_PI / 2)};
+
+	printf("dist : %f\n", dist);
+	printf("dir X : %f\ndir Y : %f\n", dir.x, dir.y);
+	printf("Sprite angle : %f\n", s_angle);
+	printf("Player angle : %f\n", p_angle);
+	printf("Diff angle : %f\n", diff_angle);
+	printf("Cam Dist : %f\n", cam_dist);
+	printf("Dot : %f\n", dot);
+	printf("SINF diff angle : %f\n", sinf(diff_angle));
+	printf("TANF diff angle : %f\n", tanf(diff_angle));
+	printf("PLANE X : %f\n", plane.x);
+	printf("PLANE y : %f\n", plane.y);
+	printf("dir X : %f\n", dir.x);
+	printf("dir y : %f\n", dir.y);
+	printf("\n");
+
 	if (cosf(diff_angle) < 0)
 		return ;
-	put_img(&cub->screen, &cub->sprite.doll, (t_position){WW / 2, WH / 2}, 2);
+	//float x = WW / 2;
+//	printf("%f\n%f\n", tanf(diff_angle) * dist, sinf());
+	float W = WW / 2;
+	float S = ft_dot(plane, udsprite);
+	//float x = W - ((W * S) * ( tanf(diff_angle) * dist) );
+	// float x = udsprite.x * cosf(-p_angle) - udsprite.y * sinf(-p_angle);
+	//float x = ft_dot(plane, udsprite);
+	printf("%f\n", S);
+	float x = W + ((W * S));
+	float y = WH / 2;
+	put_img(&cub->screen, &cub->sprite.woman, (t_position){x, y},
+				(1 / dist));
 }
