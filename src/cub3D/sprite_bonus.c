912f5cecde81ac856e4c89b75b1ef8e11bf7d1fd
Ledda Bryan <louchebem06@gmail.com>
2021-11-02 17:19:49 +0100

    Sprite bonus norm update

--- src/cub3D/sprite_bonus.c
+++ src/cub3D/sprite_bonus.c
@@ -6,12 +6,48 @@
 /*   By: bledda <bledda@student.42nice.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/17 23:59:13 by bledda            #+#    #+#             */
-/*   Updated: 2021/11/02 15:17:03 by bledda           ###   ########.fr       */
+/*   Updated: 2021/11/02 17:19:07 by bledda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../header/sprite_bonus.h"
 #include <pthread.h>
+
+static void	fill(t_draw_sprite_thread *thread)
+{
+	thread->i = -1;
+	while (++thread->i <= ceilf(thread->value.scale))
+	{
+		if (thread->xx + thread->i >= 0
+			&& thread->xx + thread->i < 1280
+			&& thread->cub->z[thread->xx + thread->i] < thread->value.dist)
+			continue ;
+		thread->j = -1;
+		while (++thread->j <= ceilf(thread->value.scale))
+			mlx_put_pixel_to_img(&thread->cub->screen,
+				thread->xx + thread->i, thread->yy + thread->j,
+				thread->color);
+	}
+}
+
+static inline int	calc_xx(const t_draw_sprite_thread *thread)
+{
+	return (thread->px
+		* thread->src->width
+		+ thread->value.center.x
+		- (thread->src->width / 2)
+		* thread->value.scale);
+}
+
+static inline int	calc_yy(const t_draw_sprite_thread *thread)
+{
+	return (thread->py
+		* thread->src->height
+		- (thread->src->height / 2)
+		* thread->value.scale
+		+ thread->value.center.y);
+}
+
 
 static void	*put_img_multithread(void *curr_thread)
 {
@@ -27,33 +63,10 @@ static void	*put_img_multithread(void *curr_thread)
 			thread->py += (thread->value.scale / thread->src->height);
 			thread->color = \
 				mlx_get_pixel_img(thread->src, thread->x, thread->y);
-			thread->xx = thread->px
-				* thread->src->width
-				+ thread->value.center.x
-				- (thread->src->width / 2)
-				* thread->value.scale;
-			thread->yy = thread->py
-				* thread->src->height
-				- (thread->src->height / 2)
-				* thread->value.scale
-				+ thread->value.center.y;
+			thread->xx = calc_xx(thread);
+			thread->yy = calc_yy(thread);
 			thread->i = -1;
-			while (++thread->i <= ceilf(thread->value.scale))
-			{
-				if (thread->xx + thread->i >= 0
-					&& thread->xx + thread->i < 1280
-					&& thread->cub->z[thread->xx + thread->i]
-					< thread->value.dist)
-					continue ;
-				thread->j = -1;
-				while (++thread->j <= ceilf(thread->value.scale))
-				{
-					mlx_put_pixel_to_img(&thread->cub->screen,
-						thread->xx + thread->i,
-						thread->yy + thread->j,
-						thread->color);
-				}
-			}
+			fill(thread);
 		}
 		thread->py = 0;
 		thread->px += (thread->value.scale / thread->src->width);
@@ -61,27 +74,29 @@ static void	*put_img_multithread(void *curr_thread)
 	pthread_exit(0);
 }
 
+#define THREAD_SPRITE 1
+
 static void	put_img(t_cub *cub, t_img *src, t_draw_sprite value)
 {
-	t_draw_sprite_thread	t[1];
-	pthread_t				thread[1];
+	t_draw_sprite_thread	t[THREAD_SPRITE];
+	pthread_t				thread[THREAD_SPRITE];
 	int						i;
 
 	if (value.scale * src->width > 2500)
 		value.scale = 2500 / src->width;
 	i = -1;
-	while (++i < 1)
+	while (++i < THREAD_SPRITE)
 	{
 		t[i] = (t_draw_sprite_thread){0};
 		t[i].cub = cub;
 		t[i].id_thread = i;
-		t[i].nb_thread = 1;
+		t[i].nb_thread = THREAD_SPRITE;
 		t[i].src = src;
 		t[i].value = value;
 		pthread_create(&thread[i], NULL, put_img_multithread, &t[i]);
 	}
 	i = -1;
-	while (++i < 1)
+	while (++i < THREAD_SPRITE)
 		pthread_join(thread[i], NULL);
 }
 
